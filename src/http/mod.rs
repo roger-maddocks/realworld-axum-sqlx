d544f5b836186c080ac11be72e5495c722f2c6f0
David Pedersen <david.pdrsn@gmail.com>
2024-01-01 19:04:04 +0100

    Setup graceful shutdown

--- src/http/mod.rs
+++ src/http/mod.rs
@@ -5,6 +5,7 @@ use sqlx::PgPool;
 use std::{
     net::{Ipv4Addr, SocketAddr},
     sync::Arc,
+    time::Duration,
 };
 use tokio::net::TcpListener;
 
@@ -40,7 +41,7 @@ pub use error::{Error, ResultExt};
 
 pub type Result<T, E = Error> = std::result::Result<T, E>;
 
-use tower_http::trace::TraceLayer;
+use tower_http::{timeout::TimeoutLayer, trace::TraceLayer};
 
 /// The core type through which handler functions can access common API state.
 ///
@@ -85,6 +86,7 @@ pub async fn serve(config: Config, db: PgPool) -> anyhow::Result<()> {
     let addr = SocketAddr::from((Ipv4Addr::UNSPECIFIED, 8080));
     let listener = TcpListener::bind(addr).await?;
     axum::serve(listener, app)
+        .with_graceful_shutdown(shutdown_signal())
         .await
         .context("error running HTTP server")
 }
@@ -96,6 +98,35 @@ fn api_router(api_context: ApiContext) -> Router {
         .merge(profiles::router())
         .merge(articles::router())
         // Enables logging. Use `RUST_LOG=tower_http=debug`
-        .layer(TraceLayer::new_for_http())
+        .layer((
+            TraceLayer::new_for_http(),
+            TimeoutLayer::new(Duration::from_secs(30)),
+        ))
         .with_state(api_context)
+}
+
+async fn shutdown_signal() {
+    use tokio::signal;
+
+    let ctrl_c = async {
+        signal::ctrl_c()
+            .await
+            .expect("failed to install Ctrl+C handler");
+    };
+
+    #[cfg(unix)]
+    let terminate = async {
+        signal::unix::signal(signal::unix::SignalKind::terminate())
+            .expect("failed to install signal handler")
+            .recv()
+            .await;
+    };
+
+    #[cfg(not(unix))]
+    let terminate = std::future::pending::<()>();
+
+    tokio::select! {
+        _ = ctrl_c => {},
+        _ = terminate => {},
+    }
 }
